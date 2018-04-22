eefd5668d0736cc13121022b5cfb8405bbadfee2
yangfan01 <yangfan01@jcgroup.com.cn>
2018-04-22 14:39:51 +0800

    fixed

--- src/schedule/crawl.js
+++ src/schedule/crawl.js
@@ -9,7 +9,8 @@ module.exports = {
     try {
         console.log('开始抓取内容')
         const browser = await puppeteer.launch({
-          headless: true
+          headless: true,
+          args : [ '-- no-sandbox ','-- disable-setuid-sandbox ' ]
         });
         const page = await browser.newPage();
         const loadCount = 10
