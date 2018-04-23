3d209d55eaa2b13f0948f886761f8493117446d8
杨帆 <incomparable9527@foxmail.com>
2018-04-23 12:34:05 +0800

    优化

--- src/schedule/index.js
+++ src/schedule/index.js
@@ -6,7 +6,7 @@ var crawl = require('./crawl');
 module.exports = function (app) {
   console.log(app.get('frontendRobot'));
   //避免第一次没有创建文件夹目录
-  crawl.init();
+  crawl.init(app);
   setTimeout(() => message.init(), 60000);
 
   (function () {
@@ -16,7 +16,7 @@ module.exports = function (app) {
     rule.hour = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
     rule.dayOfWeek = [1, 2, 3, 4, 5];
     schedule.scheduleJob(rule, function () {
-      message.init();
+      message.init(app);
     });
   })();
 
@@ -27,7 +27,7 @@ module.exports = function (app) {
     rule.second = 0;
     rule.minute = 0;
     schedule.scheduleJob(rule, function () {
-      crawl.init();
+      crawl.init(app);
     });
   })();
 };
