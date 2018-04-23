36d478b904aa986c1260f1ec100e2ea6d0b2ba2c
杨帆 <incomparable9527@foxmail.com>
2018-04-23 13:10:52 +0800

    fixed

--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,5 @@ const ds = require('./ds/ds.service.js');
 // const deploy = require('./deploy/deploy.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  app.configure(ds);
+  // app.configure(ds);
 };
